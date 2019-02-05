7e252c7a1bcbe17af4cd57997b172e149e5c5009
rizzu1023 <mrizwan9022@gmail.com>
2019-02-05 15:55:46 +0530

    data_sci

---
+++ matplotlib/plot.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x=[12,43,19,20]
+y=[7,21,33,49]
+
+x1=[10,33,50,22,7,20]
+y1=[12,10,1,23,1,20]
+# plt.plot(x,y,color='red')
+# plt.plot(x,y,'r*',color='blue',label='boys')
+# plt.plot(x1,y1,color='black',label='girls')
+
+# plt.bar(x,y,color='orange')
+# plt.bar(x1,y1,color='pink')
+
+# plt.scatter(x1,y1,color="red")
+
+a=[10,10,10,10]
+b=['A','B','C','D']
+
+# plt.pie(a,labels=b)
+# plt.hist(x,y)
+
+plt.title("INFO")
+plt.xlabel("marks")
+plt.ylabel("Student")
+plt.legend()
+plt.show()
+
